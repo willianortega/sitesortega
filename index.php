e51cd54a95a4ceea143cedf861b220b0551cceec
willianortega <willian_ortega@hotmail.com>
2015-12-27 19:07:44 -0200

    index.php
    index do site

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title>Página Inicial</title>
+	<base href="http://uk.zyro.com/data/s/i/sitesortega.com.br/out/" />
+			<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<meta name="description" content="" />
+	<meta name="keywords" content="" />
+		<meta name="generator" content="Zyro - Website Builder" />
+	
+	<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+	<link href="css/bootstrap-responsive.min.css" rel="stylesheet" type="text/css" />
+	<script src="js/jquery-1.8.3.min.js" type="text/javascript"></script>
+	<script src="js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="js/main.js" type="text/javascript"></script>
+
+	<link href="css/site.css?v=1.0.15" rel="stylesheet" type="text/css" />
+	<link href="css/common.css?ts=1451250399" rel="stylesheet" type="text/css" />
+	<link href="css/1.css?ts=1451250399" rel="stylesheet" type="text/css" />
+	<link rel="shortcut icon" href="http://uk.zyro.com/data/s/i/sitesortega.com.br/out/gallery/sitesortega-ts1451170320.png" type="image/png" />
+	<script type="text/javascript">var currLang = '';</script>		
+	<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+	<!--[if lt IE 9]>
+	  <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+
+
+<body><div class="root"><div class="vbox wb_container" id="wb_header">
+	
+<div id="wb_element_instance0" class="wb_element"><a class="btn btn-default btn-collapser"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></a><ul class="hmenu"><li><a href="Teste/" target="_self" title="Teste">Teste</a></li></ul><script type="text/javascript"> (function() { var isOpen = false, elem = $('#wb_element_instance0'), btn = elem.children('.btn-collapser').eq(0); btn.on('click', function() { if (elem.hasClass('collapse-expanded')) { isOpen = false; elem.removeClass('collapse-expanded'); } else { isOpen = true; elem.addClass('collapse-expanded'); } }); elem.find('ul').each(function() { var ul = $(this); if (ul.parent('li').length > 0) { ul.parent('li').eq(0).children('a').on('click', function() { if (!isOpen) return true; if (ul.css('display') !== 'block') ul.css({display: 'block'}); else ul.css({display: ''}); return false; }); } }); })(); </script></div></div>
+<div class="vbox wb_container" id="wb_main">
+	
+<div id="wb_element_instance1" class="wb_element"><div></div></div><div id="wb_element_instance2" class="wb_element"><img alt="" src="gallery_gen/0c862147475d6f28b3e1f0dbc571a366_70x70.png"></div><div id="wb_element_instance3" class="wb_element"><div></div></div><div id="wb_element_instance4" class="wb_element" style=" line-height: normal;"><h1 class="wb-stl-heading1" style="text-align: center;"><span style="color:#801f80;">Em Construção</span></h1>
+</div><div id="wb_element_instance5" class="wb_element"><img alt="" src="gallery_gen/db74b9b85623cbe20fbe215574fa1615_761x423.png"></div><div id="wb_element_instance7" class="wb_element" style="width: 100%;">
+						<script type="text/javascript">
+				$(function() {
+					$("#wb_element_instance7").hide();
+				});
+			</script>
+						</div></div>
+<div class="vbox wb_container" id="wb_footer">
+	
+<div id="wb_element_instance6" class="wb_element"><a class="wb_button" href="Index/"><span>Apenas faça!</span></a></div></div><div class="wb_sbg"></div></div></body>
+</html>
